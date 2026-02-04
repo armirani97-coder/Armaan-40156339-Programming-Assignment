a91ba988e9ed797e9eec62ca0068630b00597f9c
armir <armir@192.168.0.145>
2026-02-04 16:12:59 +0000

    First Commit

---
+++ src/main/java/com/mycompany/assignmentprogramming40156339/AssignmentProgramming40156339.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.assignmentprogramming40156339;
+
+/**
+ *
+ * @author armir
+ */
+public class AssignmentProgramming40156339 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
